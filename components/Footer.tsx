b2948f8cfbf294f08d055d98645645e034151187
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-09-01 16:26:08 +0100

    suprememods

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 import Divider from "./Divider"
 
 const Footer = () => {
@@ -8,9 +9,9 @@ const Footer = () => {
                 <Image src="/logo.png" width={125} height={125} />
             </div>
             <div className="max-w-3xl flex items-center justify-between space-x-5">
-                <div><Image src="/twitter.png" width={50} height={50} /></div>
-                <div><Image src="/instagram.png" width={50} height={50} /></div>
-                <div><Image src="/spotify.png" width={50} height={50} /></div>
+                <Link href="/"><a><Image src="/twitter.png" width={50} height={50} /></a></Link>
+                <Link href="/"><a><Image src="/instagram.png" width={50} height={50} /></a></Link>
+                <Link href="/"><a><Image src="/spotify.png" width={50} height={50} /></a></Link>
             </div>
             <Divider />
             <div className="pb-2.5">
