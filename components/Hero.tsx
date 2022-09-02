53a5f44128e978d5c18c7a106a750c026c2920af
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-09-02 06:38:33 +0100

    suprememods

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,10 @@ const Hero = () => {
         <div className="flex justify-center px-2.5 sm:px-16 pt-10">
             <div className="w-full space-y-5 flex flex-col md:flex-row items-center md:space-y-0 space-x-0 md:space-x-5 md:max-w-4xl">
                 <div className="space-y-6">
-                    <h1 className="text-3xl text-center font-bold text-ash">
+                    <h1 className="text-3xl lg:text-6xl text-center lg:text-left font-bold text-ash">
                         Lorem ipsum dolor sit met, <span className="text-green">consectetur</span> adipiscing elit
                     </h1>
-                    <p className="text-lg text-center text-black">
+                    <p className="text-lg lg:text-2xl text-center lg:text-left text-black">
                         Lorem ipsum dolor sit amet, 
                         consectetur adipiscing elit ut
                         aliquam, purus sit amet
