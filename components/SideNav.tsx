1339b45b82706d3eba93e4c0e81ae228424ad02b
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-09-02 16:57:23 +0100

    suprememods

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,23 +1,37 @@
 import Link from "next/link"
 
-const SideNav = () => {
+interface Page {
+    page: string;
+}
+
+const SideNav = ({ page }: Page) => {
     return (
-        <nav className="min-h-screen fixed w-64 border-r border-ash z-20
-            bg-white transform -translate-x-full transition duration-200 ease-in-out sidebar md:hidden
-            space-y-5 px-3 py-5">
+        <nav className="fixed z-20 w-64 min-h-screen px-3 py-5 space-y-5 transition duration-200 ease-in-out transform -translate-x-full bg-white border-r border-ash sidebar md:hidden">
             <div>
                 <Link href="/">
                     <a>
-                        <div className="bg-ash text-white py-2 px-6 rounded-full text-center">
+                        <div className="px-6 py-2 text-center text-white rounded-full bg-ash">
                             <button>Login</button>
                         </div>
                     </a>
                 </Link>
             </div>
             <div className="space-y-2.5">
-                <div className="hover:bg-lightBg w-full hover:cursor-pointer p-3"><h3 className="text-lg font-bold"><Link href="/"><a>Home</a></Link></h3></div>
-                <div className="hover:bg-lightBg w-full hover:cursor-pointer p-3"><h3 className="text-lg"><Link href="/"><a>Art</a></Link></h3></div>
-                <div className="hover:bg-lightBg w-full hover:cursor-pointer p-3"><h3 className="text-lg"><Link href="/"><a>Initiative</a></Link></h3></div>
+                <div className="w-full p-3 hover:bg-lightBg hover:cursor-pointer">
+                    <h3 className={`text-lg ${page == "home" && "font-bold"}`}>
+                        <Link href="/"><a>Home</a></Link>
+                    </h3>
+                </div>
+                <div className="w-full p-3 hover:bg-lightBg hover:cursor-pointer">
+                    <h3 className={`text-lg ${page == "art" && "font-bold"}`}>
+                        <Link href="/art"><a>Art</a></Link>
+                    </h3>
+                </div>
+                <div className="w-full p-3 hover:bg-lightBg hover:cursor-pointer">
+                    <h3 className={`text-lg ${page == "init" && "font-bold"}`}>
+                        <Link href="/"><a>Initiative</a></Link>
+                    </h3>
+                </div>
             </div>
         </nav>
     )
