5cf52c92954663aa6c32acb101c78a56867ce1a0
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-09-06 05:30:07 +0100

    suprememods

--- pages/art.tsx
+++ pages/art.tsx
@@ -4,8 +4,24 @@ import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import Newsletter from "../components/Newsletter"
 import ProductList from "../components/ProductList"
+import { sanityClient } from "../sanity"
 
-const Art = () => {
+interface Data {
+    items: [
+      {
+        _id: string
+        description: string
+        thumbnail: {
+          asset: {
+            _ref: string
+          }
+        }
+        price: number
+      }
+    ]
+  }
+
+const Art = ({ items }: Data) => {
     return(
         <div>
             <Head>
@@ -17,7 +33,7 @@ const Art = () => {
             <main className="absolute inset-x-0 top-0 h-full mt-24 space-y-8">
                 <div className="w-full px-2.5 sm:px-16 lg:px-10 py-10 lg:py-20 flex flex-col items-center justify-center space-y-8">
                     <h1 className="text-2xl font-semibold text-black">Available <span className="text-green">Arts</span></h1>
-                    <ProductList page={"art"} />
+                    <ProductList page={"art"} data={items} />
                 </div>
                 <Newsletter />
                 <Footer />
@@ -27,3 +43,20 @@ const Art = () => {
 }
 
 export default Art
+
+export const getServerSideProps = async() => {
+    const query = `
+      *[_type == "items"]{
+        _id,
+        thumbnail,
+        description,
+        price
+      }
+    `
+    const items = await sanityClient.fetch(query)
+    return {
+      props: {
+        items
+      }
+    }
+  }
