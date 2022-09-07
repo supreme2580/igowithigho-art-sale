4205907c3d2a3a8672b7c7cf7e965ffcab2f5321
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-09-07 15:51:14 +0100

    suprememods

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -25,7 +25,8 @@ const ProductList = ({ page, data }: Page) => {
     return(
         <div className={`flex ${page == "art" && "flex-col items-center justify-center space-y-4"} lg:space-y-0 w-full max-w-4xl ${page == "home" && "space-x-4"} overflow-x-scroll lg:grid lg:grid-cols-3 lg:gap-4 lg:space-x-0 xl:w-auto scrollbar-hide`}>
             {
-                data.map(item => <Card key={item._id} image={urlFor(item.thumbnail.asset._ref).url()} description={item.description} id={item._id} slug={item.slug.current} price={item.price} title={item.title} />)
+                //render everyother card but the first cart because it has been renderd already on the hero component
+                data.map((item, index) => index != 0 ? <Card key={item._id} image={urlFor(item.thumbnail.asset._ref).url()} description={item.description} id={item._id} slug={item.slug.current} price={item.price} title={item.title} /> : "")
             }
         </div>
     )
