0b785005e9a72740c2e52ea955ce46390ee568b8
Victor Omorogbe <victoromorogbe69@gmail.com>
2022-09-08 16:11:14 +0100

    suprememods

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -1,4 +1,4 @@
-import Navbar from "../../components/Navbar"
+import Navbar from "../../../components/Navbar"
 import Head from "next/head"
 import { getProviders, signIn as SignIntoProvider } from "next-auth/react"
 
