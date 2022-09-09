132bbd9989e32be6e52981d4cc67910df199b83f
Supreme Labs <100731397+supreme2580@users.noreply.github.com>
2022-09-09 10:02:22 +0100

    suprememods

--- sanity.js
+++ sanity.js
@@ -5,7 +5,7 @@ const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     apiVersion: "v2021-10-21",
-    useCdn: process.env.NODE_ENV === "production"
+    useCdn: false
 }
 
 export const sanityClient = createClient(config)
